57fb30dbe7d503c980d1c8a97247f12eca7f2edf
youngff43 <youngff43@gmail.com>
2022-05-24 12:25:04 -0400

    Updated Ignore Files

---
+++ index.js
@@ -0,0 +1,8 @@
+// requirements
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const validator = require("email-validator");
+
